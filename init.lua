2b63630e178a82283ddecd17dfde37d03e3e6b99
Roland Scheumann <ros2@gmx.net>
2019-08-16 04:23:24 +0200

    finally fixed bed

--- init.lua
+++ init.lua
@@ -6,18 +6,18 @@ beds.register_bed("darkageobjects:bunk_bed", {
 	tiles = {
 		bottom = {
      --up (+Y), down (-Y), right (+X), left (-X), back (+Z), front (-Z).
-			"beds_bunkbed_bottom_top.png",
+			"beds_bunkbed_bottom_top.png^[transformR90",
 			"beds_bunkbed_under.png",
 			"beds_bunkbed_bottom_side.png",
-			"beds_bunkbed_bottom_side.png^[transformFX",
+			"beds_bunkbed_bottom_side.png",
 			"beds_bunkbed_bottom_foot.png",
 			"beds_bunkbed_bottom_head.png",
 		},
 		top = {
-			"beds_bunkbed_top_top.png",
+			"beds_bunkbed_top_top.png^[transformR90",
 			"beds_bunkbed_under.png",
 			"beds_bunkbed_top_side.png",
-			"beds_bunkbed_top_side.png^[transformFX",
+			"beds_bunkbed_top_side.png",
 			"beds_bunkbed_top_foot.png",
             "beds_bunkbed_top_head.png",
 		}
@@ -36,16 +36,18 @@ beds.register_bed("darkageobjects:bunk_bed", {
 			{-0.375, -0.25, 0.1875, 0.375, -0.125, 0.5}, -- Kissen
 		},
 		top = {
-			{-0.5, -0.5, 0.4375, -0.375, 0.1875, 0.5},
-			{0.375, -0.5, 0.4375, 0.5, 0.1875, 0.5},
-			{-0.5, 0, 0.4375, 0.5, 0.125, 0.5},
-			{-0.5, -0.375, 0.4375, 0.5, -0.125, 0.5},
-			{-0.5, -0.375, -0.5, -0.4375, -0.125, 0.5},
-			{0.4375, -0.375, -0.5, 0.5, -0.125, 0.5},
-			{-0.4375, -0.3125, -0.5, 0.4375, -0.0625, 0.4375},
+			{-0.5, -0.375, -0.5, -0.375, -0.125, 0.375}, -- Brett_rechts
+			{0.375, -0.375, -0.5, 0.5, -0.125, 0.375}, -- Brett_links
+			{0.375, -0.5, 0.375, 0.5, 0.5, 0.5}, -- Pfosten_links
+			{-0.5, -0.5, 0.375, -0.375, 0.5, 0.5}, -- Pfosten_rechts
+			{-0.5, -0.375, 0.375, 0.5, -0.125, 0.5}, -- Brett_vorne
+			{-0.5, 0.25, 0.375, 0.5, 0.375, 0.5}, -- Leiste_vorn_oben
+			{-0.5, 0, 0.375, 0.5, 0.125, 0.5}, -- Leiste_vorn_unten
+			{-0.5, -0.375, -0.5, 0.5, -0.25, 0.5}, -- Boden
+			{-0.375, -0.25, -0.5, 0.375, -0.125, 0.375}, -- Mittelkissen
 		},
 	},
-	selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.06, 1.5},
+	selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 1.5},
 	recipe = {
 		{"group:stick", "group:stick", "group:stick"},
 		{"wool:white", "wool:white", "wool:white"},
